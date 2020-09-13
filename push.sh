62d872a8c6de4964c771c7401080e9973cc75928
josef-cech-acc <josef.cech@accolade.com>
2020-09-13 15:52:12 +0200

    Update  push

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
+pipenv run pip freeze > requirements.txt
 git add .
 git commit -m "$1"
-pipenv run pip freeze > requirements.txt
 ssh-agent bash -c 'ssh-add ~/.ssh/id_rsa_private; git push origin'
